94f83557b72ef556359af1a8e417aac88cb075be
David Augustynski <daugustynsk@cps.edu>
2024-12-10 11:16:56 -0600

    part 1 py

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -1,22 +1,38 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 
-#imports and formats the data
-data = pd.read_csv("part3-multivariable-linear-regression/car_data.csv")
-x = data[["miles","age"]].values
-y = data["Price"].values
-
-#split the data into training and testing data
-
-#create linear regression model
-
-#Find and print the coefficients, intercept, and r squared values. 
-#Each should be rounded to two decimal places. 
+data = pd.read_csv("part1-linear-regression/blood_pressure_data.csv")
+x = data["Age"].values
+y = data["Blood Pressure"].values
 
 
-#Loop through the data and print out the predicted prices and the 
-#actual prices
-print("***************")
-print("Testing Results")
+x = x.reshape(-1,1)
+
+model= LinearRegression().fit(x,y)
+coef= (round(float(model.coef_[0])),2)
+intercept = round(float(model.intercept), 2)
+r_squared = model.score(x,y)
+x_predict = 43
+prediction = model.predict([[x_predict]])
+
+
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
+print(f"Prediction when x is {x_predict}: {prediction}")
+
+
+plt.figure(figsize=(6,4))
+
+plt.scatter(x,y, c="purple")
+plt.scatter(x_predict, prediction, c="blue")
+
+plt.xlabel("Temperature °F")
+plt.ylabel("Chirps per Minute")
+plt.title("Cricket Chirps by Temperature")
+
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+plt.legend()
+plt.show()
